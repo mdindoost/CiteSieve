0936757c26712895c00af105c756bec97224338f
mdindoost <mohammad.dindoost@gmail.com>
2025-11-11 20:37:57 -0500

    feat: strict title matching, config fallback, py38 hints, tests passing

---
+++ src/citesieve/__init__.py
@@ -0,0 +1,12 @@
+# src/citesieve/__init__.py
+__all__ = [
+    "cli",
+    "filters",
+    "fetch_openalex",
+    "fetch_semanticscholar",
+    "normalize",
+    "reporting",
+    "resolver",
+    "utils",
+]
+__version__ = "0.1.0"
